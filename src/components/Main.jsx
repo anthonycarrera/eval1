dfc56d30d4da0d5d8e44ca101d0285d087ee0f41
Anthony <anthony.carrera@pucp.pe>
2021-07-30 08:32:23 -0500

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+
+
+
+const Main = () => {
+    return(
+<div>
+<p>Hola mundo</p>
+    <img width="30%" src="/images/logoppc.png" alt="logo-papercube"/>
+
+</div>
+   
+
+    )
+
+}
+
+export default Main
